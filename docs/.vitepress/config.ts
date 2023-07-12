a168893237aa05f7e5d8e821877247610a0347af
Qzai <857617704@qq.com>
2023-07-12 19:14:34 +0800

    222

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
-  base:'/docs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
